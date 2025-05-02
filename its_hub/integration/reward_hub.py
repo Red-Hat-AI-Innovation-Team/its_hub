8c4b82bbaa6de39a1f22faf186bae0b1787d0076
Kai Xu <xuk@redhat.com>
2025-05-02 20:06:28 +0000

    feat: support drsow
    Signed-off-by: Kai Xu <xuk@redhat.com>

--- its_hub/integration/reward_hub.py
+++ its_hub/integration/reward_hub.py
@@ -1,16 +1,39 @@
 from typing import List, Union
 
+import numpy as np
+
 from reward_hub.base import AggregationMethod
+from reward_hub import AutoRM
+from reward_hub.drsow import DrSowConfig
 
 from ..base import AbstractOutcomeRewardModel, AbstractProcessRewardModel
+
+def sigmoid(x):
+    # use numpy's clip to avoid overflow in the exponential
+    return 1.0 / (1.0 + np.exp(-np.clip(x, -88.0, 88.0)))
 
 class LocalVllmProcessRewardModel(AbstractProcessRewardModel):
     def __init__(self, model_name: str, device: str, aggregation_method: AggregationMethod):
         from reward_hub.vllm.reward import VllmProcessRewardModel
+        if "drsow" in model_name:
+            strong_model_name, weak_model_name = model_name.split("+")[1:]
 
-        self.model = VllmProcessRewardModel(
-            model_name=model_name, device=device
-        )
+            drsow_config = DrSowConfig(
+                strong_model_name=strong_model_name,
+                strong_port=8001,
+                weak_model_name=weak_model_name,
+                weak_port=8002, 
+            )
+
+            self.model = AutoRM.load("drsow", load_method="openai", drsow_config=drsow_config)
+            self.score_scale = 1.0 / 0.03
+            self.func = sigmoid
+        else:
+            self.model = VllmProcessRewardModel(
+                model_name=model_name, device=device
+            )
+            self.score_scale = 1.0
+            self.func = lambda x: x
         self.aggregation_method = aggregation_method
 
     def score(self, prompt: str, steps: Union[List[str], List[List[str]]]) -> float:
@@ -24,6 +47,7 @@ class LocalVllmProcessRewardModel(AbstractProcessRewardModel):
             aggregation_method=self.aggregation_method,
             return_full_prm_result=False,
         )
+        res = [self.func(r) * self.score_scale for r in res]
         if is_single_prompt:
             return res[0]
         else:
